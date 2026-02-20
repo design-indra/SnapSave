6b165e32ba1ce6f6efefc722283273d755a88f3e
design-indra <ghostersoloist@gmail.com>
2026-02-21 01:13:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,22 +10,25 @@ def get_video_data(link):
         response = requests.get(api_url, params=params)
         data = response.json()
         if data.get('code') == 0:
-            # Mengambil link video tanpa watermark
-            return data['data']['play']
+            return {
+                'video': data['data']['play'],
+                'cover': data['data']['cover']
+            }
         return None
     except:
         return None
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    video_url = None
+    result = None
     error = None
     if request.method == 'POST':
         input_link = request.form.get('url')
-        video_url = get_video_data(input_link)
-        if not video_url:
+        result = get_video_data(input_link)
+        if not result:
             error = "Gagal mengambil video. Pastikan link valid."
-    return render_template('index.html', video_url=video_url, error=error)
+    return render_template('index.html', result=result, error=error)
 
-# Penting untuk Vercel
-app.debug = True
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
